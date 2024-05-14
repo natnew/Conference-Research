84fcb3690562dc42fa05b25ec26c057a729f93a3
Natasha <natashanewboldlondon@gmail.com>
2024-05-14 07:50:35 +0100

    Create imports.py

---
+++ con_research/src/modules/imports.py
@@ -0,0 +1,28 @@
+import os
+import re
+import pandas as pd
+from langchain_openai import ChatOpenAI
+import requests
+from bs4 import BeautifulSoup
+from io import BytesIO, StringIO
+import urllib.request
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.pdfpage import PDFPage
+import getpass
+from typing import Optional, Type, Any
+from pydantic import BaseModel, Field
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+import json
+import sys
+import time
+from langchain.prompts import PromptTemplate
+from langchain.llms import OpenAI
+import streamlit as st
+import base64
