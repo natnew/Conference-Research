fcdde77e560931b91aa07bf58d79326f05d2bff4
Tom Odhiambo <110313204+Jaimboh@users.noreply.github.com>
2024-05-23 14:16:32 +0300

    Update BioGen.py

--- BioGen.py
+++ BioGen.py
@@ -15,8 +15,6 @@ with st.sidebar:
     st.markdown(
        "This tool is a work in progress. "
             )
-    
-    #openai_api_key = st.text_input("OpenAI API Key", key="file_qa_api_key", type="password")
     "[View the source code](https://github.com/natnew/Conference-Research/RAG.py)"
     "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
 
