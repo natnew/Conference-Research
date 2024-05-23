3e37fbe3a1bdf9821b069a04a62d57f7efcaf163
Tom Odhiambo <110313204+Jaimboh@users.noreply.github.com>
2024-05-23 14:11:42 +0300

    Update 2_RAG.py

--- pages/2_RAG.py
+++ pages/2_RAG.py
@@ -9,7 +9,8 @@ with st.sidebar:
     st.markdown(
        "This tool is a work in progress. "
             )
-    openai_api_key = st.text_input("OpenAI API Key", key="file_qa_api_key", type="password")
+    # openai_api_key = st.text_input("OpenAI API Key", key="file_qa_api_key", type="password")
+    openai_api_key = st.secrets["openai_api_key"]
     "[View the source code](https://github.com/natnew/Conference-Research/RAG.py)"
     "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
 
