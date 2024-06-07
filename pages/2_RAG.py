36f465c71bf649cd0a951c2a550bf57145b040dc
Natasha <natashanewboldlondon@gmail.com>
2024-06-07 21:44:21 +0100

    Update 2_RAG.py

--- pages/2_RAG.py
+++ pages/2_RAG.py
@@ -1,38 +1,39 @@
-import streamlit as st
 from openai import OpenAI
-import pandas as pd
+import streamlit as st
 
 with st.sidebar:
     st.markdown("# About")
+    st.markdown(
+       "We use multi-agent systems and other AI technologies to power this app. "
+            )
+    st.markdown(
+       "This tool is a work in progress. "
+            )
+    openai_api_key = st.secrets["openai_api_key"]
+    "[View the source code](https://github.com/natnew/Conference-Research/RAG.py)"
+    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
 
-# Process the uploaded file and question
-if uploaded_file and question:
-    try:
-        if uploaded_file.name.endswith('.txt') or uploaded_file.name.endswith('.md'):
-            # Attempt to read the file with UTF-8 encoding
-            pass
-        
-        prompt = f"Here's an article:\n\n{article}\n\nQuestion: {question}\nAnswer:"
+st.title("💬 RAG")
+st.markdown("Search database")
 
-        # Set OpenAI API key
-        openai.api_key = openai_api_key
 
-        # Call OpenAI API to get the response
-        response = openai.ChatCompletion.create(
-            model="gpt-4",
-            messages=[
-                {"role": "system", "content": "You are a helpful assistant."},
-                {"role": "user", "content": prompt},
-            ],
-            max_tokens=100,
-            n=1,
-            stop=None,
-            temperature=0,
-        )
+#st.caption("🚀 A Streamlit chatbot powered by OpenAI")
 
-        # Display the response
-        st.write("### Answer")
-        st.write(response.choices[0].message["content"].strip())
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
 
-    except Exception as e:
-        st.error(f"An error occurred: {e}")
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
+
+if prompt := st.chat_input():
+    if not openai_api_key:
+        st.info("Please add your OpenAI API key to continue.")
+        st.stop()
+
+    client = OpenAI(api_key=openai_api_key)
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.chat_message("user").write(prompt)
+    response = client.chat.completions.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
+    msg = response.choices[0].message.content
+    st.session_state.messages.append({"role": "assistant", "content": msg})
+    st.chat_message("assistant").write(msg)
