89e6bce2c5ac787a3a371260a04dad43dfef161e
Natasha <natashanewboldlondon@gmail.com>
2024-06-08 10:49:00 +0100

    Update 3_Outreach.py

--- pages/3_Outreach.py
+++ pages/3_Outreach.py
@@ -1,36 +1,18 @@
 import streamlit as st
 from langchain.llms import OpenAI
+st.set_page_config(page_title="🦜🔗 Quickstart App")
+st.title('🦜🔗 Quickstart App')
 
-# Sidebar information
-with st.sidebar:
-    st.markdown("# About")
-    st.markdown(
-        "We use multi-agent systems and other AI technologies to power this app."
-    )
-    st.markdown(
-        "This tool is a work in progress."
-    )
-    openai_api_key = st.secrets["openai_api_key"]
-    st.markdown("[View the source code](https://github.com/natnew/Conference-Research/RAG.py)")
-    st.markdown("[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)")
-
-st.title("💬 Lead Generation")
-st.markdown("Craft emails to participants to reach out and engage with them before the conference.")
+openai_api_key = st.sidebar.text_input('OpenAI API Key')
 
 def generate_response(input_text):
-    try:
-        llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
-        response = llm(input_text)
-        st.info(response)
-    except Exception as e:
-        st.error(f"Error generating response: {e}")
+  llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
+  st.info(llm(input_text))
 
 with st.form('my_form'):
-    text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
-    submitted = st.form_submit_button('Submit')
-    
-    if submitted:
-        if not openai_api_key.startswith('sk-'):
-            st.warning('Please enter your OpenAI API key!', icon='⚠')
-        else:
-            generate_response(text)
+  text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
+  submitted = st.form_submit_button('Submit')
+  if not openai_api_key.startswith('sk-'):
+    st.warning('Please enter your OpenAI API key!', icon='⚠')
+  if submitted and openai_api_key.startswith('sk-'):
+    generate_response(text)
