501bc6bc957c9eaf8280e271577b4bd9036aa36d
Natasha <natashanewboldlondon@gmail.com>
2024-09-10 14:07:12 +0100

    Update BioGen.py

--- BioGen.py
+++ BioGen.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import yaml
+import pandas as pd
 from yaml.loader import SafeLoader
 from con_research.src.modules.scrapping_module import ContentScraper
 from con_research.src.modules.search_module import SerperDevTool
