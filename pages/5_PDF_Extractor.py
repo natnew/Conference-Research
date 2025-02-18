6cbe2bd677df398882540a7ca84cfd136c2a5712
Tom Odhiambo <110313204+Jaimboh@users.noreply.github.com>
2025-02-18 08:30:51 +0300

    Update 5_PDF_Extractor.py

--- pages/5_PDF_Extractor.py
+++ pages/5_PDF_Extractor.py
@@ -51,7 +51,7 @@ class ExtractionResponse(BaseModel):
 def extract_text_from_pdf(pdf_path):
     pdf_document = fitz.open(pdf_path)
     total_pages = pdf_document.page_count
-    pages_to_process = [0, 1, 2,3,4,5,6,7,8,9,10]
+    pages_to_process = [0, 1, 2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]
     extracted_texts = []
 
     for page in pages_to_process:
